92ab1a984c4e24049ae5a6964e343f3bb3db62d3
Daniel Nogueira <113999917+danielzinhonog@users.noreply.github.com>
2022-12-07 11:54:54 -0300

    JWT
    Criação de um Token.

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+//CRIAÇÃO DE TOKEN JWT (QUE SERÁ REUTILIZADO NO FUTURO)
+function base64UrlEncode($data){
+    return str_replace(['+', '/', '='], ['-','_',''], base64_Encode($data));
+}
+//CABEÇALHO
+$header = base64UrlEncode(
+    '{
+        "alg":"HS256", 
+        "typ": "JWT"
+    }'
+);
+//VARIÁVEL QUE ARMAZENARÁ DADOS DO USUÁRIO (LOGIN, SENHA, CHAVE PIX)
+$payload = base64UrlEncode(
+    '{
+        "sub":"'.md5(time()).'", 
+        "name": "Daniel Nogueira", 
+        "iat": '.time().'
+    }'
+);
+//ASSINATURA
+$signature = base64UrlEncode(
+    hash_hmac('sha256', $header.'.'.$payload, 'confirma'. true)
+);
+echo $header.'.'.$payload.'.'.$signature;
+?>
